a9703bdeede7d1145e80c0560a51f824a96e3097
loganballard <33067676+loganballard@users.noreply.github.com>
2019-11-14 15:51:20 -0800

    almost got this doing cool stuff, still need to make hashtag collections and figure out the processing of tweets -> hashtags, then how to insert programmatically

--- process_csvs.py
+++ process_csvs.py
@@ -1,36 +1,52 @@
 from csv import DictReader
 from os import listdir, getcwd, path
 from pymongo import MongoClient
-MONGO_HOST = 'localhost'
-MONGO_PORT = 27017
-
-client = MongoClient(host=MONGO_HOST, port=MONGO_PORT, appname='test_scripts')
+from yaml import load, BaseLoader
+from pprint import pprint
 
 
-def create_database():
-    mongo_db = client.tweet_db
-    tweet_collection = mongo_db.tweet_collection
+def make_collections(config, db):
+    collection_map = {}
+    return collection_map
 
 
+def process_hashtags_in_tweets(row):
+    hashtag_map = {}
+    return hashtag_map
 
-def get_info_and_insert_into_collection(filepath):
-    list_to_insert = []
+
+def insert_into_default_collection(collection, list_to_insert):
+    pass
+
+
+def get_info_and_insert_into_collection(filepath, config, db):
+    def_list_to_insert = []
+    hashtag_list_to_insert = []
+    collection_map = make_collections(config, db)
+    default_collection = collection_map.get('default').get('name', 'tweets')
+    hashtag_collection = collection_map.get('hashtags').get('name', 'hashtags')
     with open(filepath, newline='', encoding='utf-8') as csvfile:
         r = DictReader(csvfile, fieldnames=None, delimiter=',', quotechar='"')
         for row in r:
-            list_to_insert.append(dict(row))
-            if len(list_to_insert) > 100:
-                result = tweet_collection.insert_many(list_to_insert)
-                print(result.inserted_ids)
-                list_to_insert = []
+            def_list_to_insert.append(dict(row))
+            if len(def_list_to_insert) > 100:
+                insert_into_default_collection(default_collection, def_list_to_insert)
+                def_list_to_insert = []
 
 
-def process_files():
-    tweets_path = getcwd() + path.sep + "tweets"
+def process_files(config):
+    client = MongoClient(host=config.get('host', 'localhost'), port=int(config.get('port', 27017)), appname='test_scripts')
+    db = client.get_database(name=config.get('db', 'tweet_db'))
+    tweets_path = getcwd() + path.sep + config.get('tweet_folder', 'tweets')
     for file_name in listdir(tweets_path):
         file_path = tweets_path + path.sep + file_name
-        get_info_and_insert_into_collection(file_path)
+        get_info_and_insert_into_collection(file_path, config, db)
+
+
+def get_config(config_file_path='config.yaml'):
+    with open(config_file_path, 'r') as config_file:
+        return load(config_file.read(), Loader=BaseLoader)
 
 
 if __name__ == '__main__':
-    process_files()
+    pprint(get_config())

66e03497917f78b1fd55bc93d82447a04a2db449
loganballard <33067676+loganballard@users.noreply.github.com>
2019-11-14 14:14:46 -0800

    updating readme

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# This is where tweets CSVs go
+# Loading the tweets into a mongo database
 
 to obtain the tweets:
 
 `$ git clone https://github.com/fivethirtyeight/russian-troll-tweets.git`
+- put them into the tweets folder
+- (probably do this in a virtual environment)
+    - `$ python3 -m virtualenv venv`
+    - `$ source venv/bin/activate`
+- `pip install -r requirements.txt`
+- create a local mongo running instance 
+    - install the `mongo` / `mongos` / `mongod` binaries
+    - `$ mongod`
+- `$ python process_csvs.py`
---
+++ process_csvs.py
@@ -0,0 +1,31 @@
+from csv import DictReader
+from os import listdir, getcwd, path
+from pymongo import MongoClient
+from threading import Thread
+
+client = MongoClient()
+mongo_db = client.tweet_db
+tweet_collection = mongo_db.tweet_collection
+
+
+def get_info_and_insert_into_collection(filepath):
+    list_to_insert = []
+    with open(filepath, newline='', encoding='utf-8') as csvfile:
+        r = DictReader(csvfile, fieldnames=None, delimiter=',', quotechar='"')
+        for row in r:
+            list_to_insert.append(dict(row))
+            if len(list_to_insert) > 100:
+                result = tweet_collection.insert_many(list_to_insert)
+                print(result.inserted_ids)
+                list_to_insert = []
+
+
+def process_files():
+    tweets_path = getcwd() + path.sep + "tweets"
+    for file_name in listdir(tweets_path):
+        file_path = tweets_path + path.sep + file_name
+        get_info_and_insert_into_collection(file_path)
+
+
+if __name__ == '__main__':
+    process_files()

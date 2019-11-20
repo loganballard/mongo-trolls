dbc6b3a9b1471c94087631e79926cda5e826c40c
loganballard <33067676+loganballard@users.noreply.github.com>
2019-11-19 23:18:17 -0800

    refactoring to a cli

---
+++ driver.py
@@ -0,0 +1,51 @@
+import argparse
+import process_csvs as pc
+
+
+def get_desc():
+    return """Use this tool to populate data in mongo.  Without any arguments, this will read configuration
+    data from config.yaml and add it to a mongodb on localhost:27017.  It will check if the collections
+    it wants to create are already populated, and if so it will quit (unless the -f flag is supplied)."""
+
+
+def populate_data(config, force=False, debug=False):
+    db = pc.get_db_from_config(config)
+    collection_candidates = pc.get_collections(config, db, debug)
+    collections = collection_candidates.copy()
+    if not force:
+        for name, collection in collection_candidates.items():
+            if collection.estimated_document_count() > 0:
+                print(f"collection {collection.name} not empty, removing!")
+                collections.pop(name)
+    pc.process_files(config, collections, debug)
+
+
+def destroy_data(config, debug=False):
+    if debug:
+        print("erasing docs from collections")
+    try:
+        pc.clear_database(config)
+        if debug:
+            print("collections cleared")
+    except Exception as e:
+        print(e.with_traceback())
+
+
+def process_args(args):
+    config_fp = args.config if args.config else "config.yaml"
+    config = pc.get_config(config_fp)
+    debug = True if args.verbose or config.get('debug', '') else False
+    if args.delete:
+        destroy_data(config, debug)
+    else:
+        populate_data(config, args.force, debug)
+
+
+parser = argparse.ArgumentParser(description=get_desc())
+parser.add_argument("-c", "--config", type=str, help="path to the config yaml (default: ./config.yaml)")
+parser.add_argument("-v", "--verbose", action="store_true", help="print verbose output")
+parser.add_argument("-d", "--delete", action="store_true", help="delete all docs and indices from all collections " +
+                                                                "in config file.  Quit afterwards")
+parser.add_argument("-f", "--force", action="store_true", help="skip population checks and force-populate data")
+args = parser.parse_args()
+process_args(args)
--- process_csvs.py
+++ process_csvs.py
@@ -2,12 +2,10 @@ from csv import DictReader
 from os import listdir, getcwd, path
 from pymongo import MongoClient, errors
 from yaml import load, BaseLoader
-from pprint import pprint
 
-DEBUG = False
 INSERT_COUNT = 0
 
-def get_collections(config, db):
+def get_collections(config, db, debug=False):
     collection_map = {}
     for col in config.get('collections', []):
         collection_name = col.get('name')
@@ -18,7 +16,8 @@ def get_collections(config, db):
             for index in col.get('indices', []):
                 mongo_collection.create_index(index)
         except errors.CollectionInvalid as e:
-            print(f"collection {collection_name} already exists")
+            if debug:
+                print(f"collection {collection_name} already exists")
             mongo_collection = db.get_collection(collection_name)
         collection_map[collection_name] = mongo_collection
     return collection_map
@@ -33,20 +32,20 @@ def process_hashtags_in_tweets(row, cache_tags):
             cache_tags[tag_without_hash] = cache_tags[tag_without_hash] + 1 if (cache_tags.get(tag_without_hash)) else 1
 
 
-def insert_dict_into_collection(collection, dict_to_insert):
+def insert_dict_into_collection(collection, dict_to_insert, debug=False):
     for key, count in dict_to_insert.items():
         collection.insert_one({
             "hashtag": key,
             "count": count
         })
-    if DEBUG:
+    if debug:
         print(f"inserted hashtag dict into: {collection.name}")
         print(f"inserted count: {len(dict_to_insert)}")
 
 
-def insert_list_into_collection(collection, list_to_insert):
+def insert_list_into_collection(collection, list_to_insert, debug=False):
     collection.insert_many(list_to_insert)
-    if DEBUG:
+    if debug:
         global INSERT_COUNT
         INSERT_COUNT += len(list_to_insert)
         print(f"inserting list into: {collection.name}")
@@ -54,18 +53,18 @@ def insert_list_into_collection(collection, list_to_insert):
         print(f"inserted count: {INSERT_COUNT}")
 
 
-
-def get_info_and_insert_into_collection(filepath, collection_map, cache_tags):
+def get_info_and_insert_into_collection(filepath, collection_map, cache_tags, debug):
     tweet_list_to_insert = []
-    default_collection = collection_map.get('tweets')
-    with open(filepath, newline='', encoding='utf-8') as csvfile:
-        r = DictReader(csvfile, fieldnames=None, delimiter=',', quotechar='"')
-        for row in r:
-            tweet_list_to_insert.append(dict(row))
-            process_hashtags_in_tweets(row=row, cache_tags=cache_tags)
-            if len(tweet_list_to_insert) > 100:
-                insert_list_into_collection(default_collection, tweet_list_to_insert)
-                tweet_list_to_insert = []
+    default_collection = collection_map.get('tweets', None)
+    if default_collection:
+        with open(filepath, newline='', encoding='utf-8') as csvfile:
+            r = DictReader(csvfile, fieldnames=None, delimiter=',', quotechar='"')
+            for row in r:
+                tweet_list_to_insert.append(dict(row))
+                process_hashtags_in_tweets(row=row, cache_tags=cache_tags)
+                if len(tweet_list_to_insert) > 100:
+                    insert_list_into_collection(default_collection, tweet_list_to_insert, debug)
+                    tweet_list_to_insert = []
 
 
 def get_db_from_config(config):
@@ -75,24 +74,20 @@ def get_db_from_config(config):
     return db
 
 
-def process_files(config):
-    db = get_db_from_config(config)
-    collection_map = get_collections(config, db)
+def process_files(config, collections_map, debug=False):
     tweets_path = getcwd() + path.sep + config.get('tweet_folder', 'tweets')
     cache_tags = {}
     for file_name in listdir(tweets_path):
         file_path = tweets_path + path.sep + file_name
-        get_info_and_insert_into_collection(file_path, collection_map, cache_tags)
-    hashtag_collection = collection_map.get('hashtags')
-    insert_dict_into_collection(hashtag_collection, cache_tags)
+        get_info_and_insert_into_collection(file_path, collections_map, cache_tags, debug)
+    hashtag_collection = collections_map.get('hashtags', None)
+    if hashtag_collection:
+        insert_dict_into_collection(hashtag_collection, cache_tags, debug)
 
 
 def get_config(config_file_path='config.yaml'):
     with open(config_file_path, 'r') as config_file:
-        config = load(config_file.read(), Loader=BaseLoader)
-        global DEBUG
-        DEBUG = config.get("debug", False)
-        return config
+        return load(config_file.read(), Loader=BaseLoader)
 
 
 def clear_database(config):
@@ -101,6 +96,3 @@ def clear_database(config):
     for col in collection_map.values():
         col.delete_many({})  # DELETES ALL DOCS!
         col.drop_indexes()
-
-if __name__ == '__main__':
-    process_files(get_config())

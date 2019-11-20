e98221433f8d0fc985410fc1af0253798b7a7d98
loganballard <33067676+loganballard@users.noreply.github.com>
2019-11-19 22:25:20 -0800

    fully operational

--- process_csvs.py
+++ process_csvs.py
@@ -4,8 +4,10 @@ from pymongo import MongoClient, errors
 from yaml import load, BaseLoader
 from pprint import pprint
 
+DEBUG = False
+INSERT_COUNT = 0
 
-def make_collections(config, db):
+def get_collections(config, db):
     collection_map = {}
     for col in config.get('collections', []):
         collection_name = col.get('name')
@@ -23,7 +25,6 @@ def make_collections(config, db):
 
 
 def process_hashtags_in_tweets(row, cache_tags):
-    cache_tags = {}
     row_dict = dict(row)
     tweet = row_dict.get('content', '')
     for word in tweet.split():
@@ -33,18 +34,30 @@ def process_hashtags_in_tweets(row, cache_tags):
 
 
 def insert_dict_into_collection(collection, dict_to_insert):
-    pprint(dict_to_insert)
+    for key, count in dict_to_insert.items():
+        collection.insert_one({
+            "hashtag": key,
+            "count": count
+        })
+    if DEBUG:
+        print(f"inserted hashtag dict into: {collection.name}")
+        print(f"inserted count: {len(dict_to_insert)}")
 
 
 def insert_list_into_collection(collection, list_to_insert):
-    pass
+    collection.insert_many(list_to_insert)
+    if DEBUG:
+        global INSERT_COUNT
+        INSERT_COUNT += len(list_to_insert)
+        print(f"inserting list into: {collection.name}")
+        print(f"first list item: {list_to_insert[0]}")
+        print(f"inserted count: {INSERT_COUNT}")
 
 
-def get_info_and_insert_into_collection(filepath, collection_map):
+
+def get_info_and_insert_into_collection(filepath, collection_map, cache_tags):
     tweet_list_to_insert = []
-    cache_tags = {}
     default_collection = collection_map.get('tweets')
-    hashtag_collection = collection_map.get('hashtags')
     with open(filepath, newline='', encoding='utf-8') as csvfile:
         r = DictReader(csvfile, fieldnames=None, delimiter=',', quotechar='"')
         for row in r:
@@ -53,23 +66,41 @@ def get_info_and_insert_into_collection(filepath, collection_map):
             if len(tweet_list_to_insert) > 100:
                 insert_list_into_collection(default_collection, tweet_list_to_insert)
                 tweet_list_to_insert = []
-    insert_dict_into_collection(hashtag_collection, cache_tags)
+
+
+def get_db_from_config(config):
+    client = MongoClient(host=config.get('host', 'localhost'), port=int(config.get('port', 27017)),
+                         appname='test_scripts')
+    db = client.get_database(name=config.get('db', 'tweet_db'))
+    return db
 
 
 def process_files(config):
-    client = MongoClient(host=config.get('host', 'localhost'), port=int(config.get('port', 27017)), appname='test_scripts')
-    db = client.get_database(name=config.get('db', 'tweet_db'))
-    collection_map = make_collections(config, db)
+    db = get_db_from_config(config)
+    collection_map = get_collections(config, db)
     tweets_path = getcwd() + path.sep + config.get('tweet_folder', 'tweets')
+    cache_tags = {}
     for file_name in listdir(tweets_path):
         file_path = tweets_path + path.sep + file_name
-        get_info_and_insert_into_collection(file_path, collection_map)
+        get_info_and_insert_into_collection(file_path, collection_map, cache_tags)
+    hashtag_collection = collection_map.get('hashtags')
+    insert_dict_into_collection(hashtag_collection, cache_tags)
 
 
 def get_config(config_file_path='config.yaml'):
     with open(config_file_path, 'r') as config_file:
-        return load(config_file.read(), Loader=BaseLoader)
+        config = load(config_file.read(), Loader=BaseLoader)
+        global DEBUG
+        DEBUG = config.get("debug", False)
+        return config
 
+
+def clear_database(config):
+    db = get_db_from_config(config)
+    collection_map = get_collections(config, db)
+    for col in collection_map.values():
+        col.delete_many({})  # DELETES ALL DOCS!
+        col.drop_indexes()
 
 if __name__ == '__main__':
-    process_files(get_config())
+    clear_database(get_config())
